469cfb027ce5926e09a7308a37fa1028f048728a
Faseun <faseunoluwatobilobasimon@gmail.com>
2024-02-27 15:59:03 +0100

    Created exeption middle ware for handling errors

---
+++ PLT-ANP-API/Extentions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Contracts;
+using Entities.ErrorModel;
+using Entities.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+
+
+namespace PLT_ANP_API.Extentions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this WebApplication app,
+            ILoggerManager logger)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    // Set the response content type to JSON
+                    context.Response.ContentType = "application/json";
+
+                    // Get the exception handler feature from the current context
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        // Set the response status code based on the type of error
+                        context.Response.StatusCode = contextFeature.Error switch
+                        {
+                            // Use the switch statement to return the appropriate error status code
+                            NotFoundException => StatusCodes.Status404NotFound,
+                            BadRequestException => StatusCodes.Status400BadRequest,
+                            //InvalidCredentialsException => StatusCodes.Status401Unauthorized,
+                            //ActivateUserException => StatusCodes.Status406NotAcceptable,
+                            //EntrustException => StatusCodes.Status400BadRequest,
+                            //Invalid_Token => StatusCodes.Status401Unauthorized,
+
+                            _ => StatusCodes.Status500InternalServerError
+                        };
+
+                        // Log the error message
+                        logger.LogError($"Something went wrong: {contextFeature.Error}");
+
+                        // Write the error details as a JSON response
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = contextFeature.Error.Message,
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
