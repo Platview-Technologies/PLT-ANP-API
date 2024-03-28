66285fcb7c20c19564d162cb2d8c594fcd7a1798
Faseun <faseunoluwatobilobasimon@gmail.com>
2024-03-28 17:50:44 +0100

    feat: Implement endpoint for refreshing authentication with cookie information
    Description:
    - Added an endpoint to refresh authentication using cookie information.
    - This feature enhances user authentication by providing the ability to refresh authentication tokens seamlessly.
    - Users can now maintain their sessions and stay authenticated across different devices using cookie-based authentication.

--- PLT_ANP_API.Presentation/AuthenticationController/TokenController.cs
+++ PLT_ANP_API.Presentation/AuthenticationController/TokenController.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using PLT_ANP_API.Presentation.ActionFilters;
 using Service.Contract;
 using Shared.DTOs;
-
+using Shared.DTOs.Response;
 
 namespace PLT_ANP_API.Presentation.AuthenticationController
 {
@@ -23,6 +24,34 @@ namespace PLT_ANP_API.Presentation.AuthenticationController
         {
             var returnToken = await _service.AuthenticationService.RefreshToken(tokenDto);
             return Ok(returnToken);
+        }
+
+        [HttpPost("refreshCookie")]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> RefreshTokenCookie([FromBody] ATokenDto aTokenDto)
+        {
+            string refreshToken = Request.Cookies["rt"];
+            if (refreshToken == null)
+            {
+                return Unauthorized();
+            }
+            var tokenDto = new TokenDto()
+            {
+                AccessToken = aTokenDto.AccessToken,
+                RefreshToken = refreshToken
+            };
+
+            var returnToken = await _service.AuthenticationService.RefreshToken(tokenDto);
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = false, // Make sure to set this to true if using HTTPS
+                SameSite = SameSiteMode.None, // Adjust as per your requirements
+                MaxAge = TimeSpan.FromDays(7) // Set the cookie's expiration time
+            };
+
+            Response.Cookies.Append("rt", returnToken.RefreshToken, cookieOptions);
+            return Ok(new ATokenDto() { AccessToken = returnToken.AccessToken });
         }
     }
 }
